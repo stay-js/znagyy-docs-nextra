ef7130911df50642eefc88ef6530f0955ac7bb89
Nagy Zétény <nagyzeteny6@gmail.com>
2025-11-09 02:28:29 +0100

    update build script, add explixit tagname, remove builder after use

--- build.sh
+++ build.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
-docker buildx create --use
+read -p "Enter tag name: " TAGNAME
+
+BUILDER_NAME=temp_builder_$(date +%s)
+docker buildx create --name $BUILDER_NAME --use
 
 docker buildx build \
   --platform linux/amd64,linux/arm64 \
+  -t znagyy/docs:$TAGNAME \
   -t znagyy/docs:latest \
   --push .
+
+docker buildx rm $BUILDER_NAME
